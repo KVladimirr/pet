655a467425f37c5312c32abee483f2b0f5a65f9d
Vladimir Korotin <vkorotin@ozon.ru>
2025-08-02 23:04:04 +0300

    pg connection func

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -1,11 +1,19 @@
 package storage
 
-// import (
-// 	"context"
+import (
+	"context"
+	"fmt"
 
-// 	"github.com/jackc/pgx/v5/pgxpool"
-// )
+	"github.com/jackc/pgx/v5"
+)
 
-// func NewPostgres(ctx context.Context, connString string) (*pgxpool.Pool, error) {
+func PgConnect(ctx context.Context, connString string) (*pgx.Conn, error) {
+	conn, err := pgx.Connect(ctx, connString)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
 
-// }
+	// defer conn.Close(ctx) Вынести в функцию меин
+
+	return conn, nil
+}
